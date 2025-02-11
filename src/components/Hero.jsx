b75387b0bdf7d010bdcb581af3042d5329136f6a
Sameer Khan A <sameerkhan003a@gmail.com>
2025-02-11 14:47:52 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Footer from "./Footer.jsx";
+import Navbar from "./Navbar.jsx";
 function Hero() {
   return (
     <>
