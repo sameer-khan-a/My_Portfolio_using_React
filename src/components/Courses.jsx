6d34eea62a9ff63ee98dfd20b54f77ed09c4c4db
Sameer Khan A <sameerkhan003a@gmail.com>
2025-02-15 17:58:21 +0530

    Update Courses.jsx

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -9,7 +9,7 @@ function Certifications(props) {
       <h2 class="display-5 fw-normal" style={{ color: "orchid" }}>
         My Certifications
       </h2>
-      <div class="row row-cols-1 row-cols-md-3 mb-3 text-center container-fluid ">
+      <div class="row skills row-cols-1 row-cols-md-3 mb-3 text-center container-fluid ">
         <Card
           title="Web Development Bootcamp by App Brewery - 2025"
           content="A comprehensive bootcamp covering essential web development skills.
