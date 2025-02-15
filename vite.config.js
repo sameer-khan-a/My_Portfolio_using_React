15f45a43e77abb7db309a5699de8ad711cb1f5b4
Sameer Khan A <sameerkhan003a@gmail.com>
2025-02-15 16:48:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,5 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
-    strictPort: true, // Ensures Vite only uses the defined port
-    allowedHosts: ["my-portfolio-using-react-ga3a.onrender.com"], // Allow your Render domain
-    cors: true // Enable CORS for cross-origin requests
   }
 });
